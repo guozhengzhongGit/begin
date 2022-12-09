5341c2f40fc17ae0f93a299b6dc317e4967bbc54
guozhengzhong <guozhengzhong@100tal.com>
2022-12-09 22:29:49 +0800

    fix: 修改 devtool

--- build/mode/index.js
+++ build/mode/index.js
@@ -5,7 +5,7 @@ const config = new Config();
 const mode = process.env.NODE_ENV=== 'test' ? 'production' : process.env.NODE_ENV
 config.mode(mode);
 
-const devTool = process.env.NODE_ENV === 'production' ? 'none' : 'cheap-module-source-map';
+const devTool = process.env.NODE_ENV === 'production' ? 'hidden-source-map' : 'cheap-module-source-map';
 config.devtool(devTool);
 
 //导出配置
